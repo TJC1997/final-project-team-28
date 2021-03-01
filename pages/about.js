277cd38f9408c2008607fd76a8234c8e8e4314b2
TJC1997 <940044928@qq.com>
2021-03-01 00:33:32 -0800

    added the navbar i found from bootstrap. Changed the fomat a bit that is close to rob hess

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,11 @@
-
+import TopNavbar from "../components/TopNavbar";
 const About = () => {
-    return (
-      <div>
-        About Page
-      </div>
-    )
-  }
-  
-  export default About;
+  return (
+    <div>
+      <TopNavbar about />
+      Our Story Page
+    </div>
+  );
+};
+
+export default About;
---
+++ pages/picturewall.js
@@ -0,0 +1,12 @@
+import TopNavbar from "../components/TopNavbar";
+
+const PictureWall = () => {
+  return (
+    <div>
+      <TopNavbar picturewall />
+      Picture Wall Page
+    </div>
+  );
+};
+
+export default PictureWall;
