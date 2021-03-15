969a9ece8d422eeb13b1ac8ac894217c15a986c4
plim1025 <plim1025@gmail.com>
2021-03-15 16:08:35 -0600

    Hid API Key

--- pages/info.js
+++ pages/info.js
@@ -1,6 +1,7 @@
 import TopNavbar from "../components/TopNavbar";
 
 const Info = () => {
+
   return (
     <div>
       <TopNavbar info />
@@ -8,7 +9,7 @@ const Info = () => {
         <h3>Wedding Info Page</h3>
         <p style={{ marginTop: 10, fontSize: 'large'}}>Wedding Date and Time: August 7, 2021 at 3PM</p>
         <p style={{ fontSize: 'large'}}>Location: East Side Baptist Church in Springfield, OR</p>
-        <iframe width="800" height="500" loading="lazy" allowFullScreen src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJe3Zbku3gwFQRez9TF21jSkg&key=AIzaSyA1J68CoZ7i5S2lufRdMNZZeSTUdK_B_-g"></iframe>
+        <iframe width="800" height="500" loading="lazy" allowFullScreen src={`https://www.google.com/maps/embed/v1/place?q=place_id:ChIJe3Zbku3gwFQRez9TF21jSkg&key=${process.env.GOOGLE_MAPS_API_KEY}`}></iframe>
       </div>
     </div>
   );
